a7246830925ce5fb6440ffff98a23d45cc7f86bb
ferranpalicio <ferran.palicio@gmail.com>
2019-05-03 23:59:12 +0200

    Add entity class + mapper extension function

---
+++ app/src/main/java/com/pal/populartv/entity/TvShow.kt
@@ -0,0 +1,9 @@
+package com.pal.populartv.entity
+
+
+data class TvShow (
+    val id: Int,
+    val name: String,
+    val imageUrl: String,
+    val score: String
+)
--- app/src/main/java/com/pal/populartv/net/dto/TvShowDto.kt
+++ app/src/main/java/com/pal/populartv/net/dto/TvShowDto.kt
@@ -1,11 +1,14 @@
 package com.pal.populartv.net.dto
 
 import com.google.gson.annotations.SerializedName
+import com.pal.populartv.entity.TvShow
+import com.pal.populartv.net.ApiConstants
 
-
-data class TvShowDto (
+data class TvShowDto(
     @SerializedName("name") val name: String,
     @SerializedName("backdrop_path") val image: String,
     @SerializedName("id") val id: Int,
     @SerializedName("vote_average") val score: String
-)
+) {
+    fun TvShowDto.toValueObject(): TvShow = TvShow(id, name, ApiConstants.BASE_IMAGE_URL + image, score)
+}
